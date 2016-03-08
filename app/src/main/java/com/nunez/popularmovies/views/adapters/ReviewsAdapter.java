48530f1808195a23d2a22b7d9204a2b7b3fe66a6
Paul Nunez <paulnunezm@gmail.com>
2016-03-08 01:29:28 -0400

    DetailActivity: added review listener, intent to open in browser, max review lines and ellipsized

--- app/src/main/java/com/nunez/popularmovies/views/adapters/ReviewsAdapter.java
+++ app/src/main/java/com/nunez/popularmovies/views/adapters/ReviewsAdapter.java
@@ -1,7 +1,9 @@
 package com.nunez.popularmovies.views.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +13,6 @@ import android.widget.TextView;
 import com.nunez.popularmovies.R;
 import com.nunez.popularmovies.model.entities.Review;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder> {
 
     private ArrayList<Review> mReviews;
+    private Context mContext;
     private Resources res;
 
     public ReviewsAdapter(ArrayList<Review> reviews) {
@@ -28,11 +30,14 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHold
 
     @Override
     public ReviewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+
+        mContext = parent.getContext();
+
+        View view = LayoutInflater.from(mContext)
                 .inflate(R.layout.item_detail_reviews, parent, false);
 
         if (res == null) {
-            res = parent.getContext().getResources();
+            res = mContext.getResources();
         }
 
         return new ViewHolder(view);
@@ -40,12 +45,20 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(ReviewsAdapter.ViewHolder holder, int position) {
-        Review review = mReviews.get(position);
+        final Review review = mReviews.get(position);
 
         holder.reviewer.setText(String.format(res.getString(R.string.format_reviewer),
                 review.getAuthor()));
-        holder.review.setText(String.format(res.getString(R.string.format_reviews),
-                review.getContent()));
+        holder.review.setText(review.getContent());
+
+        holder.container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(review.getUrl()));
+                mContext.startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -57,9 +70,11 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHold
 
         private TextView reviewer;
         private TextView review;
+        private View container;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            container = itemView;
             reviewer = (TextView) itemView.findViewById(R.id.text_reviewer);
             review = (TextView) itemView.findViewById(R.id.text_review);
         }
